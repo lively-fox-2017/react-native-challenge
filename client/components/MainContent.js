c8f3a54e882fa45448ae9744f17f69f5b43030b8
dimasgardenia <dimas.gardenia@gmail.com>
2017-11-22 00:02:48 +0700

    add refactor code and styling

--- client/components/MainContent.js
+++ client/components/MainContent.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   Text,
-  ScrollView
+  View,
+  ScrollView,
+  Image
 } from 'react-native'
-
+import { Card } from 'react-native-elements'
+import FitImage from 'react-native-fit-image';
 import axios from 'axios'
+
 
 export default class MainContent extends Component {
   constructor () {
@@ -30,12 +34,20 @@ export default class MainContent extends Component {
   render() {
     return (
       <ScrollView styles={styles.contentContainer}>
+        <Card title="Your News Today From Google APi" style={ {backgroundColor: 'blue'} }>
         { this.state.news.length == 0 ?
             <Text>Loading ....</Text> :
             this.state.news.articles.map((data, i) => {
-              return <Text key={i}>{ data.description }</Text>
+              return (
+                <View key={i}>
+                <FitImage  source={{ uri: data.urlToImage }} style={styles.fitImage} ></FitImage>
+                <Text style={{fontWeight: 'bold' }} > { data.title } </Text>
+                <Text> { data.description } </Text>
+              </View>
+              )
             })
         }
+      </Card>
       </ScrollView>
     );
   }
@@ -44,8 +56,14 @@ export default class MainContent extends Component {
 const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    textAlign: 'justify',
+    padding: 20
+  },
+  fitImage: {
+    borderRadius: 20,
+  },
+  fitImageWithSize: {
+    height: 100,
+    width: 30,
   },
 });
