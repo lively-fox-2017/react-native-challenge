dcce53b65a534d3c3b80b2d3dd80d0ba10e6076a
Indraprahastha <indraprahastha@rocketmail.com>
2017-11-22 20:05:22 +0700

    Akhirnya Done || API & Navigation

--- App.js
+++ App.js
@@ -1,69 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
-import axios from 'axios'
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+import HomeScreen from './components/Lihat'
+import HomeScreendua from './components/Lihatdua'
+
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Homedua: { screen: HomeScreendua }
+});
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      dataApi: []
-    }
-  }
-
-  componentWillMount () {
-    let self = this
-    axios.get('https://newsapi.org/v1/articles?source=ars-technica&sortBy=top&apiKey=080e457774e54e00b8fd9315ed37c24d')
-    .then(function (response) {
-      const dataApi = response.data.articles
-      self.setState({dataApi})
-      console.log(self.state.dataApi)
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    console.log('bisa karna biasa', self.state.dataApi)
-  }
-
   render() {
-    return (
-      <View style={styles.container}>
-       <ScrollView contentContainerStyle={styles.contentContainer}>
-        {this.state.dataApi.map((data, key) => {
-          return (
-            <View style={[styles.pembungkus]} key={key}>
-              <View style={[styles.kontainer]}>
-                <Text style={[styles.judul]}>{data.title}</Text>
-                <Text style={[styles.description]}>{data.description}</Text>
-              </View>
-              <View style={[styles.kontainer]}>
-                <Image source={{uri: data.urlToImage}} style={{width: 150, height: 100}} />
-              </View>
-            </View>
-          )
-        })}
-         </ScrollView>
-      </View>
-    );
+    return <SimpleApp />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: '5',
     flex: 1,
     backgroundColor: '#fff',
-  },
-  judul: {
-    fontSize: 15,
-  },
-  contentContainer: {
-    paddingVertical: 50,
-  },
-  pembungkus: {
-    flexDirection: 'row',
-  },
-  kontainer: {
-    flex: 1,
-    flexDirection:'column',
-  },
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
---
+++ components/Lihatdua.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Hyperlink from 'react-native-hyperlink';
+
+export default class HomeScreendua extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome'
+  };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: this.props.navigation.state.params.data
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+      <View style={[styles.kontainer]}>
+        <Text style={[styles.judul]}>{this.state.data.title}</Text>
+        <Text style={{paddingVertical: 10, paddingHorizontal: 10,}}>{this.state.data.author}</Text>
+        <Text style={{paddingVertical: 10, paddingHorizontal: 10,}}>{this.state.data.publishedAt}</Text>
+
+        <View style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingVertical: 20,
+          paddingHorizontal: 20,
+        }}>
+        <Image source={{uri: this.state.data.urlToImage}} style={{width: 250, height: 150, alignItems: 'center',}} />
+
+        </View>
+
+        <Text style={{paddingVertical: 10, paddingHorizontal: 10,}}>{this.state.data.description}</Text>
+
+        <Hyperlink linkDefault={ true }>
+          <Text style={ { paddingVertical: 10, paddingHorizontal: 10, fontSize: 15, } }>
+            {this.state.data.url}
+          </Text>
+        </Hyperlink>
+      </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+  },
+  kontainer: {
+  },
+  judul: {
+    fontSize: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  }
+});
